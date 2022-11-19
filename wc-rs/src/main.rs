0de910786d0c5eb280c355a3790b55bdf8f14293
calinanca99 <calinanca99@gmail.com>
2022-11-19 16:23:28 +0100

    Define `Cli` struct

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "wc-rs")]
+#[command(version = "1.0")]
+#[command(about = "wc-rs: wc (partially) ported to Rust")]
+
+struct Cli {
+    #[arg(help = "FILEs to read")]
+    file: Vec<String>,
+
+    #[arg(short = 'c', long = "bytes", help = "Print the byte counts")]
+    bytes: bool,
+
+    #[arg(short = 'm', long = "chars", help = "Print the character counts")]
+    chars: bool,
+
+    #[arg(short = 'l', long = "lines", help = "Print the newline counts")]
+    lines: bool,
+
+    #[arg(short = 'w', long = "words", help = "Print the word counts")]
+    words: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    dbg!(cli);
 }
