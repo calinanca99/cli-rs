1eecb7a56ad01217e7bc35009d16da699d950af1
calinanca99 <calinanca99@gmail.com>
2022-11-20 12:05:51 +0100

    Refactor

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -24,31 +24,20 @@ struct Cli {
     words: bool,
 }
 
-fn get_bytes(path: &str) -> usize {
-    let mut file = File::open(path).expect("File not found");
-    let mut s = String::new();
-
-    let bytes = file.read_to_string(&mut s).expect("Cannot read from file");
-
-    bytes
+fn get_bytes(s: &str) -> usize {
+    s.len()
 }
 
-fn get_words(path: &str) -> usize {
-    let mut file = File::open(path).expect("File not found");
-    let mut s = String::new();
-
-    let _ = file.read_to_string(&mut s).expect("Cannot read from file");
-
-    s.as_str().split_whitespace().collect::<Vec<_>>().len()
+fn get_words(s: &str) -> usize {
+    s.split_whitespace().collect::<Vec<_>>().len()
 }
 
-fn get_lines(path: &str) -> usize {
-    let mut file = File::open(path).expect("File not found");
-    let mut s = String::new();
+fn get_lines(s: &str) -> usize {
+    s.split('\n').collect::<Vec<_>>().len() - 1
+}
 
-    let _ = file.read_to_string(&mut s).expect("Cannot read from file");
-
-    s.as_str().split('\n').collect::<Vec<_>>().len() - 1
+fn print_count(file_lines: usize, file_words: usize, file_bytes: usize, file_path: &str) {
+    println!("{file_lines} {file_words} {file_bytes} {file_path}");
 }
 
 fn main() {
@@ -59,23 +48,26 @@ fn main() {
     let mut bytes = 0;
 
     for file_path in &cli.file {
-        let file = File::open(file_path).expect("File not found");
+        let mut file = File::open(file_path).expect("File not found");
 
         let file_metadata = file.metadata().unwrap();
 
         if file_metadata.is_dir() {
             println!("wc-rs: {file_path}: Is a directory");
-            println!("0 0 0 {file_path}");
+            print_count(0, 0, 0, file_path);
         } else {
-            let file_lines = get_lines(file_path);
-            let file_words = get_words(file_path);
-            let file_bytes = get_bytes(file_path);
+            let mut s = String::new();
+            file.read_to_string(&mut s).expect("Cannot read file");
+
+            let file_lines = get_lines(&s);
+            let file_words = get_words(&s);
+            let file_bytes = get_bytes(&s);
 
             lines += file_lines;
             words += file_words;
             bytes += file_bytes;
 
-            println!("{file_lines} {file_words} {file_bytes} {file_path}");
+            print_count(file_lines, file_words, file_bytes, file_path);
         }
     }
 
